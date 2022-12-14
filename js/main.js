4700a63802ab96e9810a1f464ffe7f45f6298c60
yunjiyeon <yangdom@naver.com>
2022-12-14 18:15:10 +0900

    첫 업로드

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$('.navBtn').click(function(){
+  $(this).toggleClass('active');
+  $('.navContainer').slideToggle(400);
+});
+
+$('.navContainer').click(function(){
+  $('.navBtn').removeClass('active');
+  $(this).slideUp(400);
+});
+
+
+var toggle = {
+  click: function (target) {
+    var $target = $(target);
+    $target.on("click", function () {
+      if ($(this).hasClass("on")) {
+        slideUp($target);
+      } else {
+        slideUp($target);
+        $(this).addClass("on").next().slideDown(400);
+      }
+
+      function slideUp($target) {
+        $target.removeClass("on").next().slideUp(400);
+      }
+    });
+  },
+};
+toggle.click(".skillsImg");
+
+
+
+/* 이미지 플러그인 / 프로젝트 */
+
+window.addEventListener("load", () => {
+  //Masonry 레이아웃(Isotope 플러그인 이용)
+  const iso = new Isotope(".projectItem", {
+    // options
+    itemSelector: "article",
+  });
+
+  const filterBtn = document.querySelectorAll(".btns>li"); //.btn>li들을 변수에
+
+  for (let el of filterBtn) {
+    //배열 filterBtn 의 아이템(갯수) 만큼 반복
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      //클릭을 할때 각 아이템(버튼)에 반복, on클라스 없애줌
+      for (let el of filterBtn) {
+        el.classList.remove("on");
+      }
+
+      //클릭한 버튼에 클라스 넣어줌
+      e.currentTarget.classList.add("on");
+
+      //클릭한 버튼에 있는 a태그 안의 속성 href의 value값을 가져온다
+      const filtering = e.currentTarget.querySelector("a").getAttribute("href");
+
+      iso.arrange({ filter: filtering }); //버튼을 누르면 필터링 작동(플러그인)
+    });
+  }
+});
+
+
