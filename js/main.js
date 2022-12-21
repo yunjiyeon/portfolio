9ff9888640176f00539286a6bd1efb9f177ec1a8
yunjiyeon <yangdom@naver.com>
2022-12-21 09:55:18 +0900

    submenu 수정

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
+const $moveText = document.querySelector(".moveText");
+
+// 글자 모음
+const letters = [
+"#도전하는",
+"#유연한", 
+"#소통하는"
+];
+
+// 글자 입력 속도
+const speed = 100;
+let i = 0;
+
+// 타이핑 효과
+const typing = async () => {  
+const letter = letters[i].split("");
+
+while (letter.length) {
+await wait(speed);
+$moveText.innerHTML += letter.shift(); 
+}
+
+// 잠시 대기
+await wait(800);
+
+// 지우는 효과
+remove();
+}
+
+// 글자 지우는 효과
+const remove = async () => {
+const letter = letters[i].split("");
+
+while (letter.length) {
+await wait(speed);
+
+letter.pop();
+$moveText.innerHTML = letter.join(""); 
+}
+
+// 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
+i = !letters[i+1] ? 0 : i + 1;
+typing();
+}
+
+// 딜레이 기능 ( 마이크로초 )
+function wait(ms) {
+return new Promise(res => setTimeout(res, ms))
+}
+
+// 초기 실행
+setTimeout(typing, 1500);
+
+
+
+
+
+/* main menu */
+
 $('.navBtn').click(function(){
   $(this).toggleClass('active');
   $('.navContainer').slideToggle(400);
@@ -8,6 +67,7 @@ $('.navContainer').click(function(){
   $(this).slideUp(400);
 });
 
+/* sub menu */
 $('.subNavBtn').click(function(){
   $(this).toggleClass('active');
   $('.subNavContainer').slideToggle(400);
@@ -174,6 +234,18 @@ $(function () {
     }
   });
 });
+
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".subNav, .subHeaderContainer h1").css("opacity", "0");
+    } else {
+      $(".subNav, .subHeaderContainer h1").css("opacity", "1");
+    }
+  });
+});
+
+
 
 $(".topBtn").click(function () {
   $("html, body").animate(
